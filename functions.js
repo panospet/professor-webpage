69fe1bcaecdb2b2c550621cbed5146bb002c5af0
panos <panospet@gmail.com>
2016-05-05 18:53:05 +0300

    moved all js functionality to a separate file

---
+++ functions.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+	
+	//container fade in
+	$('.container').hide().fadeIn(1500);
+
+	//scrolling to page section
+	$('.nav').find('ul').find('a').click(function(event){
+		event.preventDefault();
+		aboveHeight = $( $.attr(this, 'href') ).offset().top - 30
+		if (aboveHeight < 0) {aboveHeight=0;}
+		$('html, body').animate({ scrollTop: aboveHeight }, 1200);
+		$("a[href='" + theID + "']").addClass("nav-active");
+	});
+
+	//highlighting menu item script
+	var aChildren = $(".nav li").children(); //all list items
+	var aArray = [];
+	for (var i=0; i < aChildren.length; i++) { //array filled with all hrefs
+		var aChild = aChildren[i];
+		var ahref = $(aChild).attr('href');
+		aArray.push(ahref);
+		} 
+
+	$(window).scroll(function(){
+		var topPos = $(window).scrollTop() + 60; // window position + height of navigation bar
+		var windowHeight = $(window).height();
+		var docHeight = $(document).height();
+
+		for (var i=0; i < aArray.length; i++) {
+			var mydiv = aArray[i];
+			var divPos = $(mydiv).offset().top; // my div height
+			var divHeight = $(mydiv).height();
+			if (topPos >= divPos && topPos < (divPos + divHeight)) {
+   				$("a[href='" + mydiv + "']").addClass("item-active");
+			} 
+			else {
+    			$("a[href='" + mydiv + "']").removeClass("item-active");
+			}
+		}
+
+		//if contact is too short
+		if(topPos + windowHeight >= docHeight) {
+			if (!$(".nav li:last-child a").hasClass("nav-active")) {
+    			var navActiveCurrent = $(".nav-active").attr("href");
+    			$("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
+    			$(".nav li:last-child a").addClass("nav-active");
+			}
+		}
+
+		 });
+
+
+	$(".readmore").click(function() {
+		$(this).parent().children(".full-article").slideToggle("300");
+
+		if ($(this).text()=="[+]"){
+			$(this).text("[-]");
+		}
+		else if ($(this).text()=="[-]"){$(this).text("[+]");}
+		}
+	);
+});
--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-g!DOCTYPE html>
+<!DOCTYPE html>
 <html>
 	<head>
 		<meta charset="UTF-8">
@@ -12,92 +12,7 @@ g!DOCTYPE html>
 
 		<link rel="shortcut icon" href="ico.png">
 
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-
-		<!-- Mathjax embedding script -->
-		<script type="text/javascript"
-  			src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML">
-		</script>
-
-		<script type="text/javascript">
-			var aboveHeight = 0;
-			$(document).ready(function(){			
-				//container fade in
-				$('.container').hide().fadeIn(1500);
-
-				//navigation bar animation
-				$(window).scroll(function(){
-					//$('.nav').css('top', $(window).scrollTop());
-				});
-
-				$('html, body').on('mousewheel' , function(){
-					//$('.nav').css('top', $(window).scrollTop());
-					});
-				
-				//scrolling to page section
-				$('.nav').find('ul').find('a').click(function(event){
-					event.preventDefault();
-					aboveHeight = $( $.attr(this, 'href') ).offset().top - 30
-					if (aboveHeight < 0) {aboveHeight=0;}
-					$('html, body').animate({ scrollTop: aboveHeight }, 1200);
-					//$('.nav').animate({ "top": $(window).scrollTop() }, 500);
-					$("a[href='" + theID + "']").addClass("nav-active");
-				});
-
-				//highlighting menu item script
-				var aChildren = $(".nav li").children(); //all list items
-    			var aArray = [];
-    			for (var i=0; i < aChildren.length; i++) { //array filled with all hrefs
-        			var aChild = aChildren[i];
-        			var ahref = $(aChild).attr('href');
-        			aArray.push(ahref);
-    				} 
-
-    			$(window).scroll(function(){
-        			var topPos = $(window).scrollTop() + 60; // window position + height of navigation bar
-        			var windowHeight = $(window).height();
-        			var docHeight = $(document).height();
-
-        			for (var i=0; i < aArray.length; i++) {
-            			var mydiv = aArray[i];
-            			var divPos = $(mydiv).offset().top; // my div height
-            			var divHeight = $(mydiv).height();
-            			if (topPos >= divPos && topPos < (divPos + divHeight)) {
-               				$("a[href='" + mydiv + "']").addClass("item-active");
-            			} 
-            			else {
-                			$("a[href='" + mydiv + "']").removeClass("item-active");
-            			}
-        			}
-
-        			//if contact is too short
-        			if(topPos + windowHeight >= docHeight) {
-            			if (!$(".nav li:last-child a").hasClass("nav-active")) {
-                			var navActiveCurrent = $(".nav-active").attr("href");
-                			$("a[href='" + navActiveCurrent + "']").removeClass("nav-active");
-                			$(".nav li:last-child a").addClass("nav-active");
-            			}
-        			}
-
-   				 });
-
-
-				$(".readmore").click(function() {
-					$(this).parent().children(".full-article").slideToggle("300");
-
-        			if ($(this).text()=="[+]"){
-        				$(this).text("[-]");
-        			}
-        			else if ($(this).text()=="[-]"){$(this).text("[+]");}
-        			}
-        		);
-
-			});
-		</script>
-
-
-<!-- end of jquery scripts-->
-
+		
 	</head>
 
 <!-- start of main body -->
@@ -217,4 +132,12 @@ g!DOCTYPE html>
 		<img id="globe-bottom" src="globe-bottom.png">
 		</div>
 	</body>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+
+	<!-- Mathjax embedding script -->
+	<script type="text/javascript" src="//cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML"></script>
+	<script type="text/javascript" src="functions.js"></script>
+
+<!-- end of jquery scripts-->
+
 </html>
